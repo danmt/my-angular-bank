a09a57ca5291793c698c4abf5b3d0e322f908cdb
Dan <danielarturomt@gmail.com>
2024-01-05 14:25:41 +0000

    refactor: process transaction with a custom signal rather than a store

--- src/app/transfer/process-transaction-modal.component.ts
+++ src/app/transfer/process-transaction-modal.component.ts
@@ -1,11 +1,26 @@
-import { Component, OnInit, effect, inject } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+  computed,
+  effect,
+  inject,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { TransactionInstruction } from '@solana/web3.js';
-import { ProcessTransactionStore } from './process-transaction.store';
+import { ConnectionStore, WalletStore } from '@heavy-duty/wallet-adapter';
+import { SendTransactionOptions } from '@solana/wallet-adapter-base';
+import {
+  Connection,
+  Transaction,
+  TransactionInstruction,
+  VersionedTransaction,
+} from '@solana/web3.js';
+import { firstValueFrom } from 'rxjs';
+import { createTransactionSender } from '../utils';
+import { ProcessTransactionSectionComponent } from './process-transaction-section.component';
 
 export interface ProcessTransactionModalData {
   transactionInstructions: TransactionInstruction[];
@@ -16,23 +31,7 @@ export interface ProcessTransactionModalData {
   template: `
     <header class="flex gap-4 items-center px-4 pt-4">
       <h2 class="grow">
-        @switch (transactionStatus()) {
-          @case ('pending') {
-            Pending Transaction
-          }
-          @case ('sending') {
-            Sending Transaction
-          }
-          @case ('confirming') {
-            Confirming Transaction
-          }
-          @case ('failed') {
-            Failed Transaction
-          }
-          @case ('confirmed') {
-            Successful Transaction
-          }
-        }
+        {{ title() }}
       </h2>
       <button (click)="onClose()" mat-icon-button [disabled]="isRunning()">
         <mat-icon> close </mat-icon>
@@ -40,90 +39,127 @@ export interface ProcessTransactionModalData {
     </header>
 
     <div class="p-4 min-w-[350px] max-w-[450px]">
-      @if (
-        transactionStatus() !== 'confirmed' && transactionStatus() !== 'failed'
-      ) {
-        <div class="flex flex-col gap-4 items-center">
-          <mat-progress-spinner
-            mode="indeterminate"
-            diameter="64"
-          ></mat-progress-spinner>
-          <p class="text-sm italic text-center">
-            Process... Do NOT reload the page.
-          </p>
-        </div>
-      } @else if (transactionStatus() === 'confirmed') {
-        <div>
-          <p class="mb-2">
-            Feel free to inspect the transaction on the Solana Explorer:
-          </p>
-          <p>
-            <a
-              [href]="
-                'https://explorer.solana.com/tx/' +
-                transactionResponse().signature
-              "
-              target="_blank"
-              class="underline text-blue-400"
-            >
-              [view in explorer]
-            </a>
-          </p>
-        </div>
-      } @else if (transactionStatus() === 'failed') {
-        <div>
-          <p class="px-4 py-1 bg-red-200 text-red-600">
-            {{ error() }}
-          </p>
-        </div>
-      }
+      <my-bank-process-transaction-section
+        [signature]="signature()"
+        [error]="error()"
+        [status]="status()"
+        [explorerUrl]="explorerUrl()"
+      ></my-bank-process-transaction-section>
     </div>
   `,
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatProgressSpinnerModule],
-  providers: [ProcessTransactionStore],
+  imports: [MatButtonModule, MatIconModule, ProcessTransactionSectionComponent],
 })
 export class ProcessTransactionModalComponent implements OnInit {
+  private readonly _injector = inject(Injector);
   private readonly _matDialogRef = inject(
     MatDialogRef<ProcessTransactionModalComponent>,
   );
   private readonly _data = inject<ProcessTransactionModalData>(MAT_DIALOG_DATA);
   private readonly _matSnackBar = inject(MatSnackBar);
-  private readonly _processTransactionStore = inject(ProcessTransactionStore);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _connectionStore = inject(ConnectionStore);
 
-  readonly transactionResponse =
-    this._processTransactionStore.transactionResponse;
-  readonly transactionStatus = this._processTransactionStore.transactionStatus;
-  readonly isRunning = this._processTransactionStore.isRunning;
-  readonly error = this._processTransactionStore.error;
-
-  readonly handleTransactionStatusUpdates = effect(() => {
-    if (this._processTransactionStore.transactionStatus() === 'confirmed') {
-      console.log(
-        '🎉 Transaction Succesfully Confirmed!',
-        '\n',
-        `https://explorer.solana.com/tx/${
-          this._processTransactionStore.transactionResponse().signature
-        }`,
-      );
-      this._matSnackBar.open('Transaction successfully confirmed.', 'close', {
-        duration: 3000,
-      });
-      this._matDialogRef.disableClose = false;
-    } else if (this._processTransactionStore.transactionStatus() === 'failed') {
-      console.error(this._processTransactionStore.error());
-      this._matSnackBar.open('An error occurred.', 'close', {
-        duration: 3000,
-      });
-      this._matDialogRef.disableClose = false;
-    } else {
-      this._matDialogRef.disableClose = true;
+  readonly transactionSender = createTransactionSender(
+    (
+      transaction: Transaction | VersionedTransaction,
+      connection: Connection,
+      options?: SendTransactionOptions,
+    ) => this._walletStore.sendTransaction(transaction, connection, options),
+  );
+  readonly error = computed(() => this.transactionSender().error);
+  readonly isRunning = computed(
+    () =>
+      this.transactionSender().status !== 'confirmed' &&
+      this.transactionSender().status !== 'failed',
+  );
+  readonly status = computed(() => this.transactionSender().status);
+  readonly signature = computed(() => this.transactionSender().signature);
+  readonly title = computed(() => {
+    switch (this.status()) {
+      case 'pending': {
+        return 'Pending Transaction';
+      }
+      case 'sending': {
+        return 'Sending Transaction';
+      }
+      case 'confirming': {
+        return 'Confirming Transaction';
+      }
+      case 'failed': {
+        return 'Failed Transaction';
+      }
+      case 'confirmed': {
+        return 'Successful Transaction';
+      }
     }
   });
+  readonly explorerUrl = computed(
+    () => `https://explorer.solana.com/tx/${this.signature()}`,
+  );
 
-  ngOnInit() {
-    this._processTransactionStore.setTransactionInstructions(
+  async ngOnInit() {
+    const connection = await firstValueFrom(this._connectionStore.connection$);
+
+    if (connection === null) {
+      throw new Error('Connection not available.');
+    }
+
+    const publicKey = await firstValueFrom(this._walletStore.publicKey$);
+
+    if (publicKey === null) {
+      throw new Error('Public Key not available.');
+    }
+
+    this.transactionSender.send(
+      connection,
+      publicKey,
       this._data.transactionInstructions,
+    );
+
+    const handleStatusUpdates = effect(
+      () => {
+        if (this.status() === 'confirmed') {
+          // Log in the console the result.
+          console.log(
+            '🎉 Transaction Succesfully Confirmed!',
+            '\n',
+            this.explorerUrl(),
+          );
+
+          // Display a toast notification.
+          this._matSnackBar.open(
+            'Transaction successfully confirmed.',
+            'close',
+            {
+              duration: 3000,
+            },
+          );
+
+          // Allow users closing the modal.
+          this._matDialogRef.disableClose = false;
+
+          // Destroy the effect.
+          handleStatusUpdates.destroy();
+        } else if (this.status() === 'failed') {
+          // Log in the console the error.
+          console.error(this.error());
+
+          // Display a toast notification.
+          this._matSnackBar.open('An error occurred.', 'close', {
+            duration: 3000,
+          });
+
+          // Allow users closing the modal.
+          this._matDialogRef.disableClose = false;
+
+          // Destroy the effect.
+          handleStatusUpdates.destroy();
+        } else {
+          this._matDialogRef.disableClose = true;
+        }
+      },
+      { injector: this._injector },
     );
   }
 
